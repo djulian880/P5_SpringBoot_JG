8dff91d067bcb6644a06cc37a683fffe7e38506f
Julien GUTTER <julien.gutter@gmail.com>
2024-06-13 09:33:34 +0300

    Initial project

---
+++ src/main/java/com/openclassrooms/P5_SpringBoot_JG/P5SpringBootJgApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P5_SpringBoot_JG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5SpringBootJgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5SpringBootJgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/P5_SpringBoot_JG/P5SpringBootJgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P5_SpringBoot_JG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P5SpringBootJgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4e6aadf6399e9d3e2ad60336c4cfc6c184360c5f
Julien GUTTER <julien.gutter@gmail.com>
2024-07-01 16:25:01 +0200

    Nettoyage classe principale

--- src/main/java/com/openclassrooms/P5_SpringBoot_JG/P5SpringBootJgApplication.java
+++ src/main/java/com/openclassrooms/P5_SpringBoot_JG/P5SpringBootJgApplication.java
@@ -1,28 +1,12 @@
 package com.openclassrooms.P5_SpringBoot_JG;
 
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import com.openclassrooms.P5_SpringBoot_JG.*;
-import com.openclassrooms.P5_SpringBoot_JG.Util.ManageRepositoriesFromFile;
-import com.openclassrooms.P5_SpringBoot_JG.controller.MedicalRecordController;
-import com.openclassrooms.P5_SpringBoot_JG.controller.PersonController;
-import com.openclassrooms.P5_SpringBoot_JG.model.FireStation;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.openclassrooms.P5_SpringBoot_JG.repository.FireStationRepository;
-import com.openclassrooms.P5_SpringBoot_JG.repository.MedicalRecordRepository;
-import com.openclassrooms.P5_SpringBoot_JG.repository.PersonRepository;
 
 @RestController
 @SpringBootApplication

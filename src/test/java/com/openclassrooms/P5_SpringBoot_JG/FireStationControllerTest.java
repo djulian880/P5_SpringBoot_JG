ec9dd5893c1d9568922cc120284c3a9be437e1ae
Julien GUTTER <julien.gutter@gmail.com>
2024-07-15 10:49:02 +0200

    Test unitaire des controllers

---
+++ src/test/java/com/openclassrooms/P5_SpringBoot_JG/FireStationControllerTest.java
@@ -0,0 +1,88 @@
+package com.openclassrooms.P5_SpringBoot_JG;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.openclassrooms.P5_SpringBoot_JG.controller.FireStationController;
+import com.openclassrooms.P5_SpringBoot_JG.controller.PersonController;
+import com.openclassrooms.P5_SpringBoot_JG.model.FireStation;
+import com.openclassrooms.P5_SpringBoot_JG.model.Person;
+import com.openclassrooms.P5_SpringBoot_JG.service.FireStationService;
+import com.openclassrooms.P5_SpringBoot_JG.service.PersonService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = FireStationController.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class FireStationControllerTest {
+
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private FireStationService fireStationService;
+
+	private void setUp() throws Exception {
+		FireStation mockFireStation = new FireStation();
+		mockFireStation.setAddress("112 Steppes Pl");
+		mockFireStation.setStation(3);
+		Mockito.when(fireStationService.getFireStation("112 Steppes Pl")).thenReturn(mockFireStation);
+	}
+
+	@Test
+	public void testGetFireStations() throws Exception {
+		mockMvc.perform(get("/firestations")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testPostFireStation() throws Exception {
+		mockMvc.perform(post("/firestation").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"address\":\"112 Steppes Pl\",\"station\":\"3\"}"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testPutFireStation() throws Exception {
+		setUp();
+		mockMvc.perform(put("/firestation").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"address\":\"112 Steppes Pl\",\"station\":\"3\"}"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testDeleteFireStation() throws Exception {
+		setUp();
+		mockMvc.perform(delete("/firestation").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"address\":\"112 Steppes Pl\",\"station\":\"3\"}"))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testDeleteFireStationNotFound() throws Exception {
+		mockMvc.perform(delete("/firestation").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"address\":\"112 Steppes Pl\",\"station\":\"3\"}"))
+				.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void testPostFireStationAlreadyExists() throws Exception {
+		setUp();
+		mockMvc.perform(post("/firestation").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"address\":\"112 Steppes Pl\",\"station\":\"3\"}"))
+				.andExpect(status().isForbidden());
+	}
+	
+	
+}
---
+++ src/test/java/com/openclassrooms/P5_SpringBoot_JG/MedicalRecordControllerTest.java
@@ -0,0 +1,106 @@
+package com.openclassrooms.P5_SpringBoot_JG;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.openclassrooms.P5_SpringBoot_JG.controller.MedicalRecordController;
+import com.openclassrooms.P5_SpringBoot_JG.model.MedicalRecord;
+import com.openclassrooms.P5_SpringBoot_JG.service.MedicalRecordService;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+@WebMvcTest(controllers = MedicalRecordController.class)
+@TestInstance(Lifecycle.PER_CLASS)
+
+public class MedicalRecordControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private MedicalRecordService medicalRecordService;
+
+	private void setUp() throws Exception {
+		MedicalRecord mockMedicalRecord = new MedicalRecord();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			Date date = dateFormat.parse("03/06/1989");
+			mockMedicalRecord.setBirthdate(date);
+		} catch (ParseException e) {
+
+		}
+		mockMedicalRecord.setAllergies(new ArrayList<String>());
+		ArrayList<String> medications = new ArrayList<String>();
+		medications.add("pharmacol:5000mg");
+		medications.add("terazine:10mg");
+		medications.add("noznazol:250mg");
+		mockMedicalRecord.setMedications(medications);
+		mockMedicalRecord.setFirstName("Jacob");
+		mockMedicalRecord.setLastName("Boyd");
+
+		Mockito.when(medicalRecordService.getMedicalRecord("Jacob", "Boyd")).thenReturn(mockMedicalRecord);
+	}
+
+	@Test
+	public void testGetMedicalRecords() throws Exception {
+		mockMvc.perform(get("/medicalRecords")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testPostMedicalRecord() throws Exception {
+		mockMvc.perform(post("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"firstName\":\"Jacob\",\"lastName\":\"Boyd\",\"birthdate\":\"1989-06-03T23:00:00.000+00:00\","
+						+ "\"medications\":[\"pharmacol:5000mg\",\"terazine:10mg\",\"noznazol:250mg\"],\"allergies\":[]}"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testPutMedicalRecord() throws Exception {
+		setUp();
+		mockMvc.perform(put("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"firstName\":\"Jacob\",\"lastName\":\"Boyd\",\"birthdate\":\"1989-06-03T23:00:00.000+00:00\","
+						+ "\"medications\":[\"pharmacol:5000mg\",\"terazine:10mg\",\"noznazol:250mg\"],\"allergies\":[]}"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testDeleteMedicalRecord() throws Exception {
+		setUp();
+		mockMvc.perform(delete("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"firstName\":\"Jacob\",\"lastName\":\"Boyd\",\"birthdate\":\"1989-06-03T23:00:00.000+00:00\","
+						+ "\"medications\":[\"pharmacol:5000mg\",\"terazine:10mg\",\"noznazol:250mg\"],\"allergies\":[]}"))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testDeleteMedicalRecordNotFound() throws Exception {
+		mockMvc.perform(delete("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"firstName\":\"Jacob\",\"lastName\":\"Boyd\",\"birthdate\":\"1989-06-03T23:00:00.000+00:00\","
+						+ "\"medications\":[\"pharmacol:5000mg\",\"terazine:10mg\",\"noznazol:250mg\"],\"allergies\":[]}"))
+				.andExpect(status().isNotFound());
+	}
+	
+	
+	@Test
+	public void testPostMedicalRecordAlreadyExists() throws Exception {
+		setUp();
+		mockMvc.perform(post("/medicalRecord").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"firstName\":\"Jacob\",\"lastName\":\"Boyd\",\"birthdate\":\"1989-06-03T23:00:00.000+00:00\","
+						+ "\"medications\":[\"pharmacol:5000mg\",\"terazine:10mg\",\"noznazol:250mg\"],\"allergies\":[]}"))
+				.andExpect(status().isForbidden());
+	}
+}
